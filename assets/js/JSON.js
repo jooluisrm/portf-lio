029a90e094dd45b55d27d4edb36965210cd05f95
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-06-20 12:36:30 -0300

    criei um "arquivo JSON" artificial, para usar de API

---
+++ assets/js/JSON.js
@@ -0,0 +1,8 @@
+const sites = [
+    {
+        titulo: "Relógio Digital", 
+        desc: "Um site feito em HTML, CSS & JS", 
+        urlGitHub: "https://github.com/jooluisrm/relogio_digital", 
+        urlSite: "https://jooluisrm.github.io/relogio_digital/"
+    },
+];
